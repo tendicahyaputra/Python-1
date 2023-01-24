077d056275d9075e7108eff2845f4f4f7090f934
Tendi Cahya Putra <tendicahyaputra172@gmail.com>
2023-01-24 23:51:51 +0700

    Add files via upload

---
+++ lat3loop.py
@@ -0,0 +1,14 @@
+nilai1 = float(input("masukan nilai ke-1: "))
+nilai2 = float(input("masukan nilai ke-2: "))
+nilai3 = float(input("masukan nilai ke-3: "))
+nilai4 = float(input("masukan nilai ke-4: "))
+nilai5 = float(input("masukan nilai ke-5: "))
+nilai6 = float(input("masukan nilai ke-6: "))
+nilai7 = float(input("masukan nilai ke-7: "))
+nilai8 = float(input("masukan nilai ke-8: "))
+nilai9 = float(input("masukan nilai ke-9: "))
+nilai10 = float(input("masukan nilai ke-10: "))
+totalnilai = nilai1+nilai2+nilai3+nilai4+nilai5+nilai6+nilai7+nilai8+nilai9+nilai10
+print(f"total nilai adalah",totalnilai)
+rata2 = totalnilai / 10
+print(f"rata-rata nilai adalah",rata2)
