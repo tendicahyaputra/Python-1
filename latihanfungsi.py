9fcd3643f41df5c97c69d59aefe74e52bb7267bf
Tendi Cahya Putra <tendicahyaputra172@gmail.com>
2023-01-24 23:52:09 +0700

    Add files via upload

---
+++ latihanfungsi.py
@@ -0,0 +1,16 @@
+print("=============Menghitung luas balok===========")
+panjang = int(input("nilai panjang :"))
+lebar   = int(input("nilai lebar :"))
+tinggi  = int(input("nilai tinggi :"))
+
+def luas_balok (panjang,lebar):
+  l = panjang * lebar
+  print ("hasilnya adalah :",l)
+
+
+luas_balok (panjang,lebar)
+
+def volume (tinggi):
+  
+
+
